6fdcffeef775d08277ae90b19f213f1f8b62c2e9
kavitharangani <tharanganikavi08@gmail.com>
2024-08-25 09:19:49 +0530

    create UserDAO

--- Internship/src/main/java/lk/ijse/internship/dao/UserDAO.java
+++ Internship/src/main/java/lk/ijse/internship/dao/UserDAO.java
@@ -1,4 +1,11 @@
 package lk.ijse.internship.dao;
 
-public interface UserDAO {
+import lk.ijse.internship.entity.UserEntity;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
+import org.springframework.stereotype.Repository;
+
+@EnableJpaRepositories
+@Repository
+public interface UserDAO extends JpaRepository<UserEntity, Integer> {
 }
