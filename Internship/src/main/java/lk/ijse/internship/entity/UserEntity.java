0725f2a035219a4e86d5949b7a67463097ad80d1
kavitharangani <tharanganikavi08@gmail.com>
2024-08-25 09:17:47 +0530

    create UserEntity

--- Internship/src/main/java/lk/ijse/internship/entity/UserEntity.java
+++ Internship/src/main/java/lk/ijse/internship/entity/UserEntity.java
@@ -1,4 +1,26 @@
 package lk.ijse.internship.entity;
 
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import javax.management.relation.Role;
+
+@AllArgsConstructor
+@NoArgsConstructor
+@Data
+@Builder
+@Entity
+@Table(name = "user")
 public class UserEntity {
+    @Id
+    private String id;
+    private String name;
+    @Column(unique = true)
+    private String email;
+    private String password;
+    @Enumerated(EnumType.STRING)
+    private Role role;
 }
